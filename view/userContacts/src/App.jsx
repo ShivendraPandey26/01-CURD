fc85813113cb4b72c430c594b1b538d7defc418a
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-06-02 23:51:06 +0530

    Create App.jsx

---
+++ view/userContacts/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyForm from './Components/MyForm'; // adjust the path if necessary
+
+const App = () => {
+  return (
+    <div>
+      <MyForm />
+    </div>
+  );
+};
+
+export default App;
